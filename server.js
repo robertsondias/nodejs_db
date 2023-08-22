3ec417b2080cfa72038de84878c9a6a8c371b894
robertsondias <robertsondias@gmail.com>
2023-08-22 19:00:32 -0300

    node_bd test

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ con.connect(function (err) {
 
     var sql = `SELECT * FROM produto`; 
     var sql2 = 'UPDATE produto SET categoria = 1 WHERE nome = "sofa"';
+    
     con.query(sql2, function(err, result) {
         if (err) throw err;
         console.log(result);
