69d052f6e86a2da51cb9e94f9472aeb48146fc2a
robertsondias <robertsondias@gmail.com>
2023-08-22 18:57:51 -0300

    node_bd

---
+++ server.js
@@ -0,0 +1,19 @@
+var mysql = require("mysql");
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rrrr",
+    database: "loja"
+})
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Conectado");
+
+    var sql = `SELECT * FROM produto`; 
+    var sql2 = 'UPDATE produto SET categoria = 1 WHERE nome = "sofa"';
+    con.query(sql2, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
